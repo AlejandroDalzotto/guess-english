72e9f0338cea792da40738c47a57545cdf365768
AlejandroDalzotto <aledalzotto15@gmail.com>
2024-04-09 13:38:00 -0300

    switch from grid to flex-box

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,20 +5,20 @@ import Link from "next/link";
 export default function HomePage() {
 
   return (
-    <main className="px-6 container mx-auto grid grid-rows-10 max-h-screen min-h-screen">
-      <header className="row-span-5 flex flex-col gap-y-8 items-center justify-center">
+    <main className="px-6 container flex flex-col mx-auto min-h-screen">
+      <header className="flex min-h-96 my-5 flex-col gap-y-8 items-center justify-center">
         <Title>
           ¡Guess <br className="md:hidden" /> English!
         </Title>
-        <p className="text-lg text-center text-balance">A simple game made for those spanish natives who wants to improve their skills in english and why not have some fun.</p>
+        <p className="text-lg md:text-center text-balance">A simple game made for those spanish natives who wants to improve their skills in english and why not have some fun.</p>
         <p className="text-lg">This game has two modes which are <strong>Guess the verb</strong> and <strong>Complete the phrase</strong>.</p>
       </header>
 
-      <section className="row-span-4 grid place-content-center">
+      <section className="min-h-96 my-5 grid place-content-center">
         <GameModes />
       </section>
 
-      <footer className="grid place-content-center">
+      <footer className="min-h-16 grid place-content-center">
 
         <p>made with ❤ by {" "}
           <Link
