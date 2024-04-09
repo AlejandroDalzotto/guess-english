a1f8b0f0b6f3b5869fa5c25de27989c276b0cf0d
AlejandroDalzotto <aledalzotto15@gmail.com>
2024-04-09 18:39:32 -0300

    enhance the pattern to highlight words

--- src/components/Phrase.tsx
+++ src/components/Phrase.tsx
@@ -1,16 +1,19 @@
 interface Props {
   text: string;
-  word: string;
+  words: string[];
 }
 
-export default function Phrase({ text, word }: Props) {
+export default function Phrase({ text, words }: Props) {
 
   const parts = text.split(" ")
 
   return (
     <p>
       {parts.map((part, i) => {
-        if (part.toLowerCase() === word.toLowerCase()) {
+
+        const regex = new RegExp(`\\b${part.replace(/[^a-zA-Z]/g, '')}\\b`, 'i');
+
+        if (words.some(word => regex.test(word))) {
           return (
             <span key={i + text + part} className="font-bold text-blue-500 dark:text-indigo-500 dark:hover:[text-shadow:1px_0_10px] shadow-blue-500 dark:shadow-indigo-500">
               {part + " "}
