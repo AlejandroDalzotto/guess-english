cb00c065d63b98f4f5fb6ec425c2ddb51f3601b4
AlejandroDalzotto <aledalzotto15@gmail.com>
2024-04-09 11:31:49 -0300

    add hover effect to phrase

--- src/components/Phrase.tsx
+++ src/components/Phrase.tsx
@@ -16,7 +16,7 @@ export default function Phrase({ text, word }: Props) {
       {parts.map((part, i) => {
         if (part.toLowerCase() === word.toLowerCase()) {
           return (
-            <span key={i + text + part} className="font-bold text-indigo-400">
+            <span key={i + text + part} className="font-bold text-blue-500 dark:text-indigo-500 dark:hover:[text-shadow:1px_0_10px] shadow-blue-500 dark:shadow-indigo-500">
               {part + " "}
             </span>
           );
