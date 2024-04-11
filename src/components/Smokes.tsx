d57320bde5294905a1a854d2662b9ebf0a34b4a8
AlejandroDalzotto <aledalzotto15@gmail.com>
2024-04-11 15:22:56 -0300

    ajust positions

--- src/components/Smokes.tsx
+++ src/components/Smokes.tsx
@@ -11,7 +11,7 @@ export const Smoke2 = () => {
   return (
     <div
       aria-hidden
-      className="-z-10 animate-fade-middle-opacity fixed top-32 right-1/3 md:right-32 bg-gradient-conic from-teal-600 via-orange-400 to-transparent rounded-full blur-3xl h-96 aspect-video"
+      className="-z-10 animate-fade-middle-opacity absolute top-32 right-1/3 md:right-32 bg-gradient-conic from-teal-600 via-orange-400 to-transparent rounded-full blur-3xl h-96 aspect-video"
     ></div>
   )
 }
@@ -20,7 +20,7 @@ export const Smoke3 = () => {
   return (
     <div
       aria-hidden
-      className="-z-10 animate-fade-middle-opacity fixed bottom-32 right-1/3 md:right-32 bg-gradient-conic from-red-600 via-amber-400 to-transparent rounded-full blur-3xl w-96 aspect-square"
+      className="-z-10 animate-fade-middle-opacity absolute top-32 left-1/3 md:right-32 bg-gradient-conic from-red-600 via-amber-400 to-transparent blur-3xl rounded-full w-40 h-[800px] rotate-45"
     ></div>
   )
 }
