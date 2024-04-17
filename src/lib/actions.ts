43ab5fc7ad6de03ba9132a7c17422854e39312e5
AlejandroDalzotto <aledalzotto15@gmail.com>
2024-04-17 17:11:02 -0300

    change array to set

--- src/lib/actions.ts
+++ src/lib/actions.ts
@@ -5,18 +5,17 @@ import type { Word } from './types';
 export const getWord = async () => {
 
   const file = await fs.readFile(process.cwd() + '/src/data/words.json', 'utf8');
-  const words: string[] = JSON.parse(file);
+  const words: Set<string> = JSON.parse(file);
 
-  const randIndex = Math.floor(Math.random() * words.length);
-  const rawWord = words[randIndex];
+  const randIndex = Math.floor(Math.random() * words.size);
+  const rawWord = Array.from(words)[randIndex];
   const word: Word = rawWord.split("").map((letter) => ({ value: letter, color: "neutral" }));
-
   return word;
 }
 
 export const isValidWord = async (word: string) => {
   const file = await fs.readFile(process.cwd() + '/src/data/words.json', 'utf8');
-  const words: string[] = JSON.parse(file);
+  const words: Set<string> = JSON.parse(file);
 
-  return words.includes(word);
+  return words.has(word);
 }
