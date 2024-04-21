835a7d73b3b6f4061978abcea71f9a86e968df54
AlejandroDalzotto <aledalzotto15@gmail.com>
2024-04-21 17:27:21 -0300

    adjust sound

--- src/components/GoBackButton.tsx
+++ src/components/GoBackButton.tsx
@@ -9,8 +9,8 @@ interface Props {
 
 export default function GoBackButton({ backTo, label }: Props) {
 
-  const [playOnDown] = useSound("/audio/pop-down.mp3", { volume: 0.5 })
-  const [playOnUp] = useSound("/audio/pop-up-on.mp3", { volume: 0.5 })
+  const [playOnDown] = useSound("/audio/pop-down.mp3", { volume: 0.3 })
+  const [playOnUp] = useSound("/audio/pop-up-on.mp3", { volume: 0.3 })
 
   const handleClick = () => {
     history.back();
