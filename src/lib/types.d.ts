46dd3cb8b93d6c504142845514c0bebb448f4a48
AlejandroDalzotto <aledalzotto15@gmail.com>
2024-04-25 18:35:06 -0300

    add documentation for types

--- src/lib/types.d.ts
+++ src/lib/types.d.ts
@@ -1,7 +1,21 @@
 import type { UUID } from "crypto";
 
+/**
+ * for `Wordle`
+ * 
+ * The colors that will be printed the cells when user writes a word.
+ * - `green` means the letter is in the word and in a corrrect position.
+ * - `yellow` means the letter is in the word but in the incorrect position.
+ * - `gray` is the default value.
+ * - `neutral` means the letter is not in the word. 
+ */
 export type CellColor = "green" | "yellow" | "gray" | "neutral";
 
+/**
+ * for `Wordle`
+ * 
+ * The object that represents one letter of the word.
+ */
 export type Letter = {
   value: string;
   color: CellColor;
@@ -9,10 +23,28 @@ export type Letter = {
 
 export type Word = Letter[]
 
+/**
+ * for `Guess the verb`
+ */
 export interface Verb {
+  /**
+   * The verb itself.
+   */
   value: string;
+  /**
+   * Most common translations of the verb.
+   */
   translations: string[];
+  /**
+   * Some example sentences.
+   */
   examplePhrases: string[];
+  /**
+   * The words that should be highlighted in the example sentences.
+   * This is because a verb can be written in different tenses and change.
+   * 
+   * For example: `add` and `added`.
+   */
   highlight: string[];
 }
 
@@ -24,28 +56,47 @@ export interface GameMode {
   available: boolean;
 }
 
+/** Custom type for slugs in **dynamic pages**. */
 export type Slug = string
 
 export type Language = "english"
 
-export type Topic = "travel"
+/**
+ * Available dialogue's topics in app.
+ */
+export enum Topic {
+  TRAVEL = "travel"
+}
 
-export type ChatText = {
+/**
+ * Represents one dialogue's line from the chat.
+ */
+export type Line = {
   text: string;
   sender: string;
 }
 
+/**
+ * Represents the object stored in json/db for one dialogue's line.
+ */
 export type Dialogue = {
   text: string;
+  /**
+   * Options available to choose from with a total of four where only one will be correct.
+   */
   options: string[];
+
+  /**
+   * The correct option to choose from.
+   */
   correct: string;
   sender: string;
 }
 
 export type DialogueSection = {
   topic: Topic;
-  label: string;
+  title: string;
   dialogues: Dialogue[];
-  final: ChatText;
+  final: Line;
   description: string;
 }
